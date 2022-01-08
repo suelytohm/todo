d4bb17a2ca81e9f303dbd23f7acd1d66523366b0
Suelytohm Oliveira <suelytohm.02@gmail.com>
2022-01-08 14:12:35 -0300

    clear

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-var bodyParser = require('body-parser');
 app.use(cors());
 const port = process.env.PORT || 3000;
-
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.json());
@@ -16,46 +11,6 @@ app.use(express.json());
 const TaskRoutes = require('./routes/TaskRoutes');
 
 app.use('/task', TaskRoutes);
-
-
-
-
-
-
-
-
-
-/*
-
-module.exports = function(){
-    var express = require('express');
-    var bodyParser = require('body-parser');
-    var app = express();
-    app.set('view engine', 'ejs');
-    app.set('views', './app/views');
-
-
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    
-    var rotas = require('../app/routers/web');
-    rotas(app);
-
-    const port = process.env.PORT || 3000;
-
-    app.listen(port, function(){
-        console.log("Servidor rodando na porta: " + port)
-    });
-};
-
-
-*/
-
-
-
-
-
-
 
 
 
