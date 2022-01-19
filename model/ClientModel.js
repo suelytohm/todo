21cd58e29c25a313b9dc51103f259eac4b708912
Suelytohm Oliveira <suelytohm.02@gmail.com>
2022-01-19 00:15:31 -0300

    Update ClientModel.js

--- model/ClientModel.js
+++ model/ClientModel.js
@@ -7,11 +7,11 @@ const ClientSchema = new Schema({
     name: {type: String,  required: true},
     description: {type: String, required: true},
     phone: {type: String, required: false},
-    rua: {type: String, required: true},
-    bairro: {type: String, required: true},
-    numero: {type: Number, required: true},
-    cidade: {type: String, required: true},
-    estado: {type: String, required: true},
+    rua: {type: String, required: false},
+    bairro: {type: String, required: false},
+    numero: {type: Number, required: false},
+    cidade: {type: String, required: false},
+    estado: {type: String, required: false},
     niver: {type: String, default: false},
     created: {type: Date, default: Date.now()}
 });
